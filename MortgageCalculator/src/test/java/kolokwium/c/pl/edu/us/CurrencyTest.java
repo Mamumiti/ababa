d0a45582480581d1f6b7e29deb2f1fbfc5574b6c
zajac <zajac@155.158.171.166>
2016-05-30 18:17:53 +0200

    3

--- MortgageCalculator/src/test/java/kolokwium/c/pl/edu/us/CurrencyTest.java
+++ MortgageCalculator/src/test/java/kolokwium/c/pl/edu/us/CurrencyTest.java
@@ -2,17 +2,25 @@ package kolokwium.c.pl.edu.us;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CurrencyTest
 {
+private Currency cur;
+	
+	@Before
+	public void setup(){
+		 cur = new Currency("USD", 2);
+	}
+	
 	@Test
 	public void convertFromPlnTest() {
-		fail("Not yet implemented");
+		assertEquals(6, cur.convertFromPLN(12), Double.MIN_VALUE);
 	}
 	
 	@Test
 	public void convertToPlnTest() {
-		fail("Not yet implemented");
+		assertEquals(6, cur.convertToPLN(3), Double.MIN_VALUE);
 	}
 }
